def4e888919a45af9324732e6eda96dda6780dd8
Hector P <wasabeto@gmail.com>
2020-04-23 11:35:05 -0500

    adding heroku configs

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-const express = require("express");
-const path = require("path");
+//Install express server
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname, "/dist/stepper-modal"));
+app.use(express.static(__dirname + '/dist/stepper-modal'));
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "/dist/stepper-modal/index.html"));
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/dist/stepper-modal/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
